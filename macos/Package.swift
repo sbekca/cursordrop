d4807af6edfb03ce2ff675465669f3fd5999aa2c
sbekca <shai@meels.com>
2026-05-02 02:11:15 -0400

    Create Package.swift

---
+++ macos/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "CursorDrop",
+    platforms: [.macOS(.v13)],
+    targets: [
+        .executableTarget(
+            name: "CursorDrop"
+        )
+    ]
+)
